9d8bdce71554860ebcd169414f64f47f650514d0
nightfoxbarricade <gautamborgohain90@gmail.com>
2016-01-16 21:01:05 +0800

    trying some more

--- PredictCARAVAN_Dichotomous.R
+++ PredictCARAVAN_Dichotomous.R
@@ -1,4 +1,17 @@
-logit_model = step(glm(CARAVAN ~ .,family = "binomial",data = train),direction = "backward")
+library(stats)
+library(psych)
+library(foreign)
+library(InformationValue)
+library(caret)
+train =  read.spss('/Users/gautamborgohain/Google Drive/Datasets/Caravan_train_0.sav',use.value.labels = FALSE,to.data.frame = TRUE)
+test =  read.spss('/Users/gautamborgohain/Google Drive/Datasets/Caravan_test_0.sav',use.value.labels = FALSE,to.data.frame = TRUE)
+
+#convert the categorical variables to dummy vars
+dmy = dummyVars("~ .", data = train, fullRank = T)
+new_train = data.frame(predict(dmy,newdata = train))
+train = new_train
+
+#logit_model = step(glm(CARAVAN ~ .,family = "binomial",data = train),direction = "backward")
 
 logit_model<- glm(CARAVAN~PBRAND+PPERSAUT+APLEZIER+PBYSTAND+PTRACTOR+PPLEZIER+PWALAND+PWAPART,data = train,family = binomial(link = "logit"))
 predicted = plogis(predict(logit_model,test,type = "response"))
@@ -6,7 +19,7 @@ optCutOff = optimalCutoff(test$CARAVAN,predicted)
 #Performance
 misClassError(test$CARAVAN,predicted,threshold = optCutOff)
 plotROC(test$CARAVAN,predicted)
-confusionMatrix(test$CARAVAN,predicted,threshold = 0.1)
-specificity(test$CARAVAN,predicted,threshold = 0.1)
-sensitivity(test$CARAVAN,predicted,threshold = 0.1)
+InformationValue::confusionMatrix(test$CARAVAN,predicted,threshold = 0.1)
+InformationValue::specificity(test$CARAVAN,predicted,threshold = 0.1)
+InformationValue::sensitivity(test$CARAVAN,predicted,threshold = 0.1)
 
--- explore.R
+++ explore.R
@@ -1,6 +1,9 @@
 library(stats)
 library(psych)
+library(foreign)
+library(ggplot2)
 train =  read.spss('/Users/gautamborgohain/Google Drive/Datasets/Caravan_train_0.sav',use.value.labels = FALSE,to.data.frame = TRUE)
+test =  read.spss('/Users/gautamborgohain/Google Drive/Datasets/Caravan_test_0.sav',use.value.labels = FALSE,to.data.frame = TRUE)
 summary(train)
 describe(train)
 
@@ -12,6 +15,22 @@ table(train$MOSTYPE,train$CARAVAN)
 sort(table(train$MOSTYPE,train$CARAVAN)[,2],decreasing = TRUE)
 sort(table(train$PPERSAUT,train$CARAVAN)[,2],decreasing = TRUE)
 subset(train,train$CARAVAN == 1,select = c("MOSTYPE","PBRAND"))
+
+qplot(PBRAND,PWAPART, data = new_train)
+ggplot(new_train,aes(x=PBRAND, y= PWAPART))+geom_line()+geom_jitter()
+hist(new_train$PBRAND, bins = 8)
+barplot(new_train$PBRAND)
+ggplot(data = new_train, aes(x= PBRAND))+geom_histogram(binwidth = 1)
+boxplot(new_train$PBRAND)
+
+plot = ggplot(data = new_train, aes(x= PBRAND,y = PWAPART))
+color = ifelse(new_train$CARAVAN == 1,'black','white')
+plot+ geom_jitter(col = color)
+
+
+pl = ggplot(data = new_train,aes(x= PBRAND,y= PWAPART))
+pl+geom_bar(fill = color,stat ="identity")
+
 
 
 #For PBRAND
